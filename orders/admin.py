f59e3a044fad1f498a43eb9e5a80a77d0a8b32ff
tqhung39 <33713001+tqhung39@users.noreply.github.com>
2019-07-11 10:01:18 +0700

    Update admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Order, OrderItem, Payment, Refund
 
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_request=False, refund_granted=True)
+
+make_refund_accepted.short_description = 'Update orders to refund granted'
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -11,8 +15,9 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',  'city', 'created','received', 'refund_request', 'refund_granted']
     list_filter = [ 'created', 'updated','received', 'refund_request', 'refund_granted']
     inlines = [OrderItemInline]
-
+    search_fields = ['refund_code']
+    actions = [make_refund_accepted]
 
 admin.site.register(Order, OrderAdmin)
-
+admin.site.register(Refund)
 admin.site.register(OrderItem)
